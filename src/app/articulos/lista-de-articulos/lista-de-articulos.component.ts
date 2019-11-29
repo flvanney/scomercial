8fe1d031a3e630a6ca1985c1f1d5378076f905f7
Bruno Nicolás Conti <bruno.n.conti@gmail.com>
2019-11-29 12:14:46 -0300

    Algunas cositas en las tablas

--- src/app/articulos/lista-de-articulos/lista-de-articulos.component.ts
+++ src/app/articulos/lista-de-articulos/lista-de-articulos.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 import { Articulo } from '../articulo';
 import { ArticulosService } from '../articulos.service';
 import { MatTableDataSource } from '@angular/material/table';
 
-export interface ArticuloInterface{
+export interface ArticuloInterface {
   nombre: string;
   familia: string;
   descripcion: string;
@@ -18,39 +19,51 @@ export interface ArticuloInterface{
 export class ListaDeArticulosComponent implements OnInit {
 
   articulos: any[] = [];
-  private articulosSub: Subscription;
 
-  displayedColumns = ['nombre', 'familia', 'descripcion'];
-  dataSource = new MatTableDataSource<ArticuloInterface>(this.articulos);
+  displayedColumns = ['nombre', 'familia', 'descripcion', 'habilitado', 'cantidad'];
+  dataSource = new MatTableDataSource<ArticuloInterface>();
 
-  constructor(private articulosService: ArticulosService) {}
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  constructor(private articulosService: ArticulosService) { }
 
   ngOnInit() {
     this.articulosService.traerArticulos();
     this.articulosService.traerArticuloListener()
       .subscribe((articulos: Articulo[]) => {
         this.articulos = articulos;
-        this.dataSource = new MatTableDataSource<ArticuloInterface>(this.articulos);
-      });}
-
+        this.dataSource = new MatTableDataSource<ArticuloInterface>(articulos);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      });
+  }
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-
-/*  traerPrecios(i: number) {
-    /* Si el usuario seleccionó un artículo se le devuelve la lista 
-    de precios que le corresponde al mismo. Sino, una lista vacía. 
-
-    const sinSeleccionar = this.pedidoForm.value.ventas[i].articulo == null ? true : false;
-
-    return sinSeleccionar ? [] : this.pedidoForm.value.ventas[i].articulo.precios;
+  estaHabilitado(siONo) {
+    return siONo == true ? 'Sí': 'No';
   }
 
-  public formatearMoneda(monto: number) {
-    return formatCurrency(monto, 'esAR', '$', 'ARS');
+  color(habilitado) {
+    return habilitado == true ? 'green' : 'red';
   }
 
-*/
+
+  /*  traerPrecios(i: number) {
+      /* Si el usuario seleccionó un artículo se le devuelve la lista 
+      de precios que le corresponde al mismo. Sino, una lista vacía. 
+  
+      const sinSeleccionar = this.pedidoForm.value.ventas[i].articulo == null ? true : false;
+  
+      return sinSeleccionar ? [] : this.pedidoForm.value.ventas[i].articulo.precios;
+    }
+  
+    public formatearMoneda(monto: number) {
+      return formatCurrency(monto, 'esAR', '$', 'ARS');
+    }
+  
+  */
 }
--- src/app/clientes/tabla-clientes/tabla-clientes.component.ts
+++ src/app/clientes/tabla-clientes/tabla-clientes.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 import { ClientesService } from '../clientes.service';
 import { Cliente } from '../cliente';
-import { Subscription } from 'rxjs';
+import { MatTableDataSource } from '@angular/material';
+
+interface ClienteInterface {
+  nombre: string;
+  apellido: string;
+  direccion: string;
+}
 
 @Component({
   selector: 'app-tabla-clientes',
@@ -11,18 +19,28 @@ import { Subscription } from 'rxjs';
 export class TablaClientesComponent implements OnInit {
 
   clientes: Cliente[] = [];
-  private clientesSub: Subscription;
+  dataSource: MatTableDataSource<ClienteInterface>;
 
   displayedColumns = ['nombre', 'apellido', 'direccion'];
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(private clientesService: ClientesService) { }
 
   ngOnInit() {
     this.clientesService.traerClientes();
-    this.clientesSub = this.clientesService.traerClienteListener()
+    this.clientesService.traerClienteListener()
       .subscribe((clientes: Cliente[]) => {
         this.clientes = clientes;
+        this.dataSource = new MatTableDataSource<ClienteInterface>(this.clientes);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
       });
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
