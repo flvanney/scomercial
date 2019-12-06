dabd8375d6410e8f66e8bb44bed25f6f7e1f7e6c
VanneyFranco <francovanney83@gmail.com>
2019-12-05 23:01:32 -0300

    Faltantes

--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -11,7 +11,47 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
 export class FacturaComponent implements OnInit {
 
   generatePdf(){
-    const documentDefinition = { content: 'Deberia andar' };
+    const documentDefinition = {
+      pageSize: 'A4',
+      content: [
+        {//Letra de la factura
+          text: 'B', style: 'letraFactura'
+        },
+        {
+          columns: [
+            {//Datos Empresa
+              width: 'auto',
+              alignment: 'left',
+              stack: [
+                'FACTURA Original',
+                'UNNOBA', 
+                'Monteagudo 2700',
+                'Pergamino Buenos Aires',
+                'Tel:2477-412345',
+                'Responsable Inscripto',]
+            },
+            {
+              width: '*',
+              alignment: 'right',
+              stack: [
+                'Nº factura:0001-000000001',
+                'Fecha: 06/12/2019',
+                'CUIT/CUIL: 23341441439',
+                'Ingresos brutos: 0000000000',
+                'Inicio de actividades: 01/01/2006']
+            },]
+
+        },
+
+      ],
+      styles: {
+        letraFactura: {
+          bold: true,
+          alignment: 'center',
+          fontSize: 40,
+        }
+      }
+    };
     pdfMake.createPdf(documentDefinition).download();
   }
 
